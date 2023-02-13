5b0cfdec4bf2eaae5e1cdd0a05603a41856ecf0e
Luo-JianG <2434559682@qq.com>
2023-02-13 23:22:45 +0800

    feat:首页构建

---
+++ components/Newsbox/Newsbox.vue
@@ -0,0 +1,64 @@
+<template>
+	<view class="">
+		<view class="content" v-for="item in 10">
+			<view class="img">
+				<image src="../../static/images/0.jpg" mode="aspectFill"></image>
+			</view>
+			<view class="text">
+				<view class="title">
+					这是一段标题这是一段标题这是一段标题这是一段标题
+				</view>
+				<view class="info">
+					<text>这是作者</text>
+					<text>999浏览</text>
+				</view>
+			</view>
+		</view>
+		
+	</view>
+</template>
+
+<script>
+	export default {
+		name:"Newsbox",
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style lang="scss">
+	.content {
+		display: flex;
+		padding: 20rpx;
+		.img {
+			width: 230rpx;
+			height: 160rpx;
+			image {
+				width: 100%;
+				height: 100%;
+			}
+		}
+		.text {
+			display: flex;
+			flex-direction: column;
+			justify-content: space-between;
+			margin-left: 20rpx;
+			//双行省略
+			.title {
+				font-size: 35rpx;
+				color: #333;
+			}
+			.info {
+				font-size: 20rpx;
+				color: #999;
+				margin-bottom: 10rpx;
+				text {
+					padding-left: 20rpx;
+				}
+			}
+		}
+	}
+</style>
---
+++ pages/index/index.vue
@@ -0,0 +1,52 @@
+<template>
+	<view class="content">
+		<scroll-view scroll-x class="scroll">
+			<view class="scrollItem" v-for="item in 10" >选项</view>
+		</scroll-view>
+		<view class="content">
+			<view class="contentItem">
+				<Newsbox></Newsbox>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	.scroll {
+		height: 100rpx;
+		background-color: #f7f8fa;
+		white-space: nowrap;
+		/deep/ ::-webkit-scrollbar {
+			width: 4px !important;
+			height: 1px !important;
+			overflow: auto !important;
+			background: transparent !important;
+			-webkit-appearance: auto !important;
+			display: block;
+			}
+		.scrollItem {
+			line-height: 100rpx;
+			display: inline-block;
+			padding: 0 20rpx;
+			font-size: 40rpx;
+			color: #333;
+		}
+		
+	}
+</style>
